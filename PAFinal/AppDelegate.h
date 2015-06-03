25d94d6a9de112a8cc8ce6c26b700e3d158f3358
Jorge Medellin <jorgemedellin@JorgeMedellin.attlocal.net>
2015-06-03 15:41:26 -0500

    Initial Commit

---
+++ PAFinal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PAFinal
+//
+//  Created by Jorge Medellin on 6/3/15.
+//  Copyright (c) 2015 Grupo Cascadia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
